c7072eb5878170e664b9b12a8b62d062f6fb3a07
x228126 <akhilesh.kumar@telusinternational.com>
2024-06-16 10:43:49 +0530

    metatag

--- src/components/common/loader/metaDecorator.jsx
+++ src/components/common/loader/metaDecorator.jsx
@@ -6,9 +6,9 @@ import mainLogo from'../../../images/anterghat.jpeg';
   return (
     <Helmet>
       <title>{title}</title>
-      <meta property="og:title" content={title} />
+      {/* <meta property="og:title" content={title} />
       <meta name="description" content={description} />
-      <meta property="og:description" content={description} />
+      <meta property="og:description" content={description} /> */}
       <meta property="og:image" content={imgSrc} />
 
       {/* <!-- Primary Meta Tags --> */}
